68fd6bf201d1ceface9856b047813d2e5d631bc3
George Mead <gmeadiv@gmail.com>
2021-08-17 16:30:08 -0700

    completed with class6 edits

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 console.log('smells like updog in here');
 
-const locationDiv = document.getElementById('locationProfiles');
+const locationsDiv = document.getElementById('locations');
 
-let hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+const hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 const seattle = {
-  location: 'Seattle',
+  name: 'Seattle',
   minCustomers: 23,
   maxCustomers: 65,
   customersPerHour: [],
@@ -27,7 +27,6 @@ const seattle = {
     // console.log(this.cookiesPerHour)
   },
   getTotalCookies: function() {
-    let totalCookies = 0
     for (let i=0; i < this.cookiesPerHour.length; i++) {
       this.totalCookies += this.cookiesPerHour[i];
     }
@@ -35,20 +34,20 @@ const seattle = {
   },
 }
 
-// this function generates a random amount of customers
+    // this function generates a random amount of customers
 function randomCustomers(min, max) {
   // takes in a min and max number of customers
   // runs a Math.random for min/max
   let number = Math.floor(Math.random() * (max - min) + min);
   // returns the amount of customers
   return number;
-}
+};
 
 // this function generates the total amount of cookies for a location
 function getCookiesPerHour(a, b) {
   let number = a * b;
   return number;
-}
+};
 
 seattle.getCustomersPerHour();
 seattle.getCookiesPerHour();
@@ -56,7 +55,7 @@ seattle.getTotalCookies();
 console.log(seattle);
 
 const tokyo = {
-  location: 'Tokyo',
+  name: 'Tokyo',
   minCustomers: 3,
   maxCustomers: 24,
   customersPerHour: [],
@@ -90,7 +89,7 @@ tokyo.getTotalCookies();
 console.log(tokyo);
 
 const dubai = {
-  location: 'Dubai',
+  name: 'Dubai',
   minCustomers: 11,
   maxCustomers: 38,
   customersPerHour: [],
@@ -124,7 +123,7 @@ dubai.getTotalCookies();
 console.log(dubai);
 
 const paris = {
-  location: 'Paris',
+  name: 'Paris',
   minCustomers: 20,
   maxCustomers: 38,
   customersPerHour: [],
@@ -158,7 +157,7 @@ paris.getTotalCookies();
 console.log(paris);
 
 const lima = {
-  location: 'Lima',
+  name: 'Lima',
   minCustomers: 2,
   maxCustomers: 16,
   customersPerHour: [],
@@ -191,51 +190,55 @@ lima.getCookiesPerHour();
 lima.getTotalCookies();
 console.log(lima);
 
-const salmonCookieLocations = [seattle, tokyo, dubai, paris, lima];
 // call get totalCookies on all locations at once
-function getAllTotalCookies() {
-  //loops through the salmon cookie locations and calls getTotalCookies on all locations
-  for (let i = 0; i < salmonCookieLocations.length; i++) {
-    let currentLocation = salmonCookieLocations[i];
-    currentLocation.getTotalCookies();
-    console.log(`${currentLocation.location} sold ${currentLocation.totalCookies} cookies today!`);
+// function getAllTotalCookies() {
+//   //loops through the salmon cookie locations and calls getTotalCookies on all locations
+//   for (let i = 0; i < salmonCookieLocations.length; i++) {
+//     let currentLocation = salmonCookieLocations[i];
+//     currentLocation.getTotalCookies();
+//     console.log(`${currentLocation.location} sold ${currentLocation.totalCookies} cookies today!`);
+//   }
+// }
+// getAllTotalCookies();
+
+function _makeElement(tag, parent, text) {
+  const element = document.createElement(tag);
+  parent.appendChild(element);
+  if (text) {
+    element.textContent = text;
   }
+  return element;
 }
-getAllTotalCookies();
+
+// function renderLocation(location) {
+//   _makeElement('article', locationsDiv, null)
+// }
+
+
 
 function renderLocation(location) {
-//   // takes in a location and puts it on the page
-//   // to create elements we use a method : createElement
-
-  // create an article
-  const articleElem = document.createElement('article');
-  locationDiv.appendChild(articleElem);
-  // create an image
-  const imgElem = document.createElement('img');
-  imgElem.src = location.photo;
-  articleElem.appendChild(imgElem);
-  // create an h2  name
-  const h2Elem = document.createElement('h2');
-  h2Elem.textContent = location.name;
-  articleElem.appendChild(h2Elem);
-  // h3 with color
-  // create a p for age
-  const ulElem = document.createElement('ul');
-  articleElem.appendChild(ulElem);
-  // create a ul
-  // loop through hoursOpen and generate a random number of customers
-  // for (let i = 0; i < location.customers.length; i++) {
-  //   let currentInterest = kitten.interests[i];
-  //   const liElem = document.createElement('li');
-  //   liElem.textContent = currentInterest;
-  //   ulElem.appendChild(liElem);
-  // }
+  let total = 0
+  const articleElem = _makeElement('article', locationsDiv, null);
+  _makeElement('h3', articleElem, location.name);
+  const ulElem = _makeElement('ul', articleElem, null);
+  for (let i = 0; i < hoursOpen.length; i++) {
+    const text = `${hoursOpen[i]}: ${location.cookiesPerHour[i]} cookies`;
+    total += location.cookiesPerHour[i];
+    _makeElement('li', ulElem, text)
+    // console.log(text);
+  }
+  let totalString = `Total: ${location.totalCookies} cookies`;
+  _makeElement('li', ulElem, totalString)
 }
 
+// renderLocation(seattle);
+
+const salmonCookieLocations = [seattle, tokyo, dubai, paris, lima];
+
 function renderAllLocations() {
-  // loops through the array and calls render on each location
   for (let i = 0; i < salmonCookieLocations.length; i++) {
-    let currentLocation = salmonCookieLocations[i];
+    const currentLocation = salmonCookieLocations[i];
+    currentLocation.getTotalCookies();
     renderLocation(currentLocation);
   }
 }
